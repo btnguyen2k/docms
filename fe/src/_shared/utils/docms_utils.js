6c3a8043004d765e8580bcc896d197ee5b451617
Thanh Nguyen <thanhn@microsoft.com>
2023-04-17 13:09:10 +1000

    Change GitHub Gist iframe to ratio-21x9

--- fe/src/_shared/utils/docms_utils.js
+++ fe/src/_shared/utils/docms_utils.js
@@ -36,7 +36,7 @@ const reUrlWithProtocol = /^([a-z]+:)/i
 function renderGithubGist(gist) {
     const srcScript = "https://gist.github.com/" + gist.trim() + ".js"
     const srcIframe = "data:text/html;charset=utf-8,&lt;head>&lt;base target='_blank' />&lt;/head>&lt;body>&lt;script src='" + srcScript + "'>&lt;/script>&lt;/body>"
-    let result = "<div class=\"ratio ratio-16x9\"><iframe title=\"GitHub Gist\" src=\"" + srcIframe + "\"></iframe></div>"
+    let result = "<div class=\"ratio ratio-21x9\"><iframe title=\"GitHub Gist\" src=\"" + srcIframe + "\"></iframe></div>"
     return result
 }
 
